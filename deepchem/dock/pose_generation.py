63ee033acae29519fcf3a280d3e72b5ee4c4cad9
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-04-13 17:52:30 -0700

    Changes

--- deepchem/dock/pose_generation.py
+++ deepchem/dock/pose_generation.py
@@ -17,6 +17,8 @@ from deepchem.utils import vina_utils
 
 logger = logging.getLogger(__name__)
 
+DATA_DIR = deepchem.utils.get_data_dir()
+
 
 class PoseGenerator(object):
   """A Pose Generator computes low energy conformations for molecular complexes.
